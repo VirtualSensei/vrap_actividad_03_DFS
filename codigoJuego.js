9560b9e408787d24d9e588845ba1de465acf33a9
VirtualSensei <74802046+VirtualSensei@users.noreply.github.com>
2020-12-04 19:46:12 -0300

    Add files via upload

---
+++ codigoJuego.js
@@ -0,0 +1,17 @@
+
+/*
+Actividad Nº: 3
+Descripción: Web Responsive BootStrap + Juego Adivinanza en JavaScrip
+ISPC: Desarrolador Full Stack
+Fecha limite entrega: 04/12/2020
+
+Alumno: Victor Raul Alberto Pedron
+mail: victor.pedron@gmail.com
+Version    : a3.0
+Revisión   : 04.12.2020
+*/
+
+
+function inicioJuego() {
+  document.getElementById("textoPuntos").innerHTML = "100";
+}
